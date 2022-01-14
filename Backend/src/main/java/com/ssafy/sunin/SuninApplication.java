e2a7c799d6ad6b1db10dfd317cddd0ae9f030774
aff4110 <aff4110@gmail.com>
2022-01-14 13:58:04 +0900

    백엔드 껍데기 등록

---
+++ Backend/src/main/java/com/ssafy/sunin/SuninApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.sunin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuninApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ssafy/sunin/SuninApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.sunin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuninApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

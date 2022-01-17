21c36f7231719dc138172c68d2e4b2722cd643ee
aff4110 <aff4110@gmail.com>
2022-01-17 15:19:16 +0900

    refactor: init swagger, spring security

---
+++ Backend/src/main/java/com/ssafy/sunin/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.ssafy.sunin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /*
+    * 스프링 시큐리티 관련 설정을 여기서 ~
+    * */
+
+}

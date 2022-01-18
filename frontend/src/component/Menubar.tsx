91a0e30d1d080bc10e0ea588f69362093d95cdf7
손민기 <smg8932@naver.com>
2022-01-18 17:11:45 +0900

    feat: login, navbar, menubar, mainpage

---
+++ frontend/src/component/Menubar.tsx
@@ -0,0 +1,43 @@
+import { Header, Menu, MenuItem } from 'semantic-ui-react'
+import Link from 'next/link'
+
+export default function Menuvar(){
+  return (
+    <>
+      <Menu vertical>
+        <MenuItem>
+          <p>회원프로필</p>
+          <p>회원이름</p>
+          <p>썬인등급</p>
+          <p><Link href="/Custom"><a>회원 정보 수정 이미지</a></Link></p>
+        </MenuItem>
+        <Menu.Item
+          name='personal'
+        >
+          <Header as='h4'><Link href="/Personal"><a>개인</a></Link></Header>
+        </Menu.Item>
+
+        <Menu.Item
+          name='coupons'
+          
+        >
+          <Header as='h4'><Link href="/Company"><a>기업</a></Link></Header>
+        </Menu.Item>
+
+        <Menu.Item
+          name='rebates'
+          
+        >
+          <Header as='h4'><Link href="/Rank"><a>랭킹</a></Link></Header>
+        </Menu.Item>
+
+        <Menu.Item
+          name='rebates'
+          
+        >
+          <Header as='h4'>채팅</Header>
+        </Menu.Item>
+        </Menu>
+    </>
+  );
+}

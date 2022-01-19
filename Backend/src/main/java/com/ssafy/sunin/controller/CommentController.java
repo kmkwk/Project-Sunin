2530b013dc2308bddbdd91275712a80c444cf062
aff4110 <aff4110@gmail.com>
2022-01-19 16:37:03 +0900

    feat: 댓글 조회, 작성, 수정 구현

--- Backend/src/main/java/com/ssafy/sunin/controller/CommentController.java
+++ Backend/src/main/java/com/ssafy/sunin/controller/CommentController.java
@@ -2,6 +2,9 @@ package com.ssafy.sunin.controller;
 
 import com.ssafy.sunin.domain.Comment;
 import com.ssafy.sunin.service.CommentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Api("댓글을 관리하는 Comment Controller")
 @RequestMapping(value = "/comment")
 @RequiredArgsConstructor
 public class CommentController {
@@ -18,24 +22,67 @@ public class CommentController {
     private final CommentService commentService;
 
     @GetMapping
-    @ApiOperation(value="댓글 컬렉션 조회")
-    public ResponseEntity<List<Comment>> loadComment() {
-        return new ResponseEntity<>(commentService.loadComment(), HttpStatus.OK);
+    @ApiOperation(value = "피드에 달린 댓글 조회")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "feedId", value = "피드 ObjectId", required = true),
+            @ApiImplicitParam(name = "commentId", value = "댓글 ObjectId", required = true)
+    })
+    public ResponseEntity<Comment> findCommentByFeed(int feedId, String commentId) {
+        return new ResponseEntity<>(commentService.findCommentById(feedId, commentId), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Comment> writeComment(String writer, String content) {
-        return new ResponseEntity<>(commentService.writeComment(writer, content), HttpStatus.OK);
+    @ApiOperation(value = "댓글 작성하기")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "feedId", value = "(#개발용#) 피드 ObjectId", required = true),
+            @ApiImplicitParam(name = "writer", value = "작성자", required = true),
+            @ApiImplicitParam(name = "content", value = "내용", required = true)
+    })
+    public ResponseEntity<Comment> writeComment(int feedId, String writer, String content) {
+        return new ResponseEntity<>(commentService.writeComment(feedId, writer, content), HttpStatus.OK);
     }
 
     @PutMapping
-    public ResponseEntity<Comment> updateComment(String writer, String content) {
-        return new ResponseEntity<>(commentService.updateComment(writer, content), HttpStatus.OK);
+    @ApiOperation(value="댓글 수정하기")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "commentId", value = "수정할 댓글 ObjectID", required = true),
+            @ApiImplicitParam(name = "content", value = "내용", required = true)
+    })
+    public ResponseEntity<Comment> updateComment(String commentId, String content) {
+        return new ResponseEntity<>(commentService.updateComment(commentId, content), HttpStatus.OK);
     }
 
     @DeleteMapping
-    public ResponseEntity<Comment> deleteComment(String writer) {
-        return new ResponseEntity<>(commentService.deleteComment(writer), HttpStatus.OK);
+    @ApiOperation(value="댓글 삭제하기")
+    @ApiImplicitParam(name = "commentId", value = "삭제할 댓글 ObjectID", required = true)
+    public ResponseEntity<Comment> deleteComment(String commentId) {
+        return new ResponseEntity<>(commentService.deleteComment(commentId), HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    @ApiOperation(value = "피드에 달린 댓글 전체 조회")
+    @ApiImplicitParam(name = "feedId", value = "피드 ObjectId", required = true)
+    public ResponseEntity<List<Comment>> findCommentsByFeed(int feedId) {
+        return new ResponseEntity<>(commentService.findCommentsByFeed(feedId), HttpStatus.OK);
+    }
+
+    @GetMapping("/count")
+    @ApiOperation(value = "피드에 달린 댓글 갯수 조회")
+    @ApiImplicitParam(name = "feedId", value = "피드 ObjectId", required = true)
+    public ResponseEntity<Integer> countCommentsByFeed(int feedId) {
+        return new ResponseEntity<>(commentService.countCommentsByFeed(feedId), HttpStatus.OK);
+    }
+
+    @GetMapping("/dev/all")
+    @ApiOperation(value = "#개발용# 댓글 컬렉션 전체 조회")
+    public ResponseEntity<List<Comment>> loadAllComment() {
+        return new ResponseEntity<>(commentService.loadAllComment(), HttpStatus.OK);
+    }
+
+    @GetMapping("/dev/string")
+    @ApiOperation(value = "#개발용# 댓글 컬렉션 ObjectId 조회")
+    public ResponseEntity<List<String>> loadAllObjectId() {
+        return new ResponseEntity<>(commentService.loadAllObjectId(), HttpStatus.OK);
     }
 
 }
--- Backend/src/main/java/com/ssafy/sunin/service/CommentService.java
+++ Backend/src/main/java/com/ssafy/sunin/service/CommentService.java
@@ -1,44 +1,74 @@
 package com.ssafy.sunin.service;
 
 import com.ssafy.sunin.domain.Comment;
-import com.ssafy.sunin.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
-    private final CommentRepository commentRepository;
+    private final MongoTemplate mongoTemplate;
 
-    public List<Comment> loadComment() {
-        return commentRepository.findAll();
+    public Comment findCommentById(int feedId, String commentId) {
+        Query query = new Query(Criteria.where("comment_feed_id").is(feedId).and("_id").is(commentId));
+        return mongoTemplate.findOne(query, Comment.class);
     }
 
-    public Comment writeComment(String writer, String content) {
+    public Comment writeComment(int feedId, String writer, String content) {
         Comment comment = Comment.builder()
+                .feedId(feedId)
                 .writer(writer)
                 .content(content)
                 .build();
-        return commentRepository.insert(comment);
+        return mongoTemplate.insert(comment);
     }
 
-
-    public Comment updateComment(String writer, String content) {
-        Query query = new Query(Criteria.where("writer").is(writer));
+    public Comment updateComment(String comment_id, String content) {
+        Query query = new Query(Criteria.where("_id").is(comment_id));
         Update update = new Update();
-        update.set("content", content);
+        update.set("comment_content", content);
+        update.set("comment_updated", true);
+        mongoTemplate.updateFirst(query, update, Comment.class);
         return null;
     }
 
-    public Comment deleteComment(String writer) {
-        Query query = new Query(Criteria.where("writer").is(writer));
+    public Comment deleteComment(String comment_id) {
+        Query query = new Query(Criteria.where("_id").is(comment_id));
+        mongoTemplate.remove(query, Comment.class);
         return null;
     }
 
+    public List<Comment> findCommentsByFeed(int feedId) {
+        Query query = new Query(Criteria.where("comment_feed_id").is(feedId));
+        return mongoTemplate.find(query, Comment.class);
+    }
+
+    public int countCommentsByFeed(int feedId) {
+        Query query = new Query(Criteria.where("comment_feed_id").is(feedId));
+        return mongoTemplate.find(query, Comment.class).size();
+    }
+
+    public List<Comment> loadAllComment() {
+        return mongoTemplate.findAll(Comment.class);
+    }
+
+    public List<String> loadAllObjectId() {
+        List<Comment> list = mongoTemplate.findAll(Comment.class);
+        List<String> result = new ArrayList<>();
+        for(Comment now : list) {
+            result.add("ObjectID : " + now.getId()
+                    + ", 내용 : " + now.getContent()
+                    + ", 작성자 : " + now.getWriter()
+                    + ", 수정여부 : " + now.isUpdated());
+        }
+        return result;
+    }
 }

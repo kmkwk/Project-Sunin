e379a800e29553751c2bedd573c8d1d576e71381
haru <jiaiha@naver.com>
2022-01-24 22:52:22 +0900

    refactor : signup

--- frontend/pages/signup/person.tsx
+++ frontend/pages/signup/person.tsx
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { Image, Form, Divider, Grid, Segment, Button, GridColumn } from 'semantic-ui-react'
 import '../../styles/signup.module.css';
 import Navbar from '../../src/component/Navbar'
 
-const DividerExampleVertical = () => (
+
+
+
+const signup =()=>{
+  
+  const [email,setEmail] = useState('');
+  const [name,setName] = useState('');
+  const [nick,setNick] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [phonenumber,setPhonenumber] = useState('');
+  const [address,setAddress] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if(password !== passwordCheck){
+      return setPasswordError(true);
+    }
+
+    console.log({
+      email,
+      name,
+      nick,
+      password,
+      passwordCheck,
+      phonenumber,
+      address
+    });
+  };
+const onChangeEmail = (e) => {
+  setEmail(e.target.value);
+};
+const onChangPassword = (e) => {
+  setPassword(e.target.value);
+};
+const onChangePasswordChk = (e) => {
+  setPasswordError(e.target.value !== password);
+  setPasswordCheck(e.target.value);
+};
+const onChangeName = (e) => {
+  setName(e.target.value);
+};
+const onChangeNick = (e) => {
+  setNick(e.target.value);
+};
+const onChangePhone = (e) => {
+  setPhonenumber(e.target.value);
+};
+const onChangeAddress = (e) => {
+  setAddress(e.target.value);
+}
+
+return (
   <>
   <Navbar />
   <Segment placeholder>
@@ -22,31 +76,31 @@ const DividerExampleVertical = () => (
     <Form>
     <Form.Field required>
       <label>이메일</label>
-      <input placeholder='Email' />
+      <input placeholder='Email' onChange={onChangeEmail} />
     </Form.Field>
     <Form.Field required>
       <label>비밀번호</label>
-      <input placeholder='Password' />
+      <input placeholder='Password' onChange={onChangPassword} />
     </Form.Field>
     <Form.Field required>
       <label>비밀번호 확인</label>
-      <input placeholder='Password' />
+      <input placeholder='Password' onChange={onChangePasswordChk} />
     </Form.Field>
     <Form.Field required>
       <label>이름</label>
-      <input placeholder='Name' />
+      <input placeholder='Name' onChange={onChangeEmail} />
     </Form.Field>
     <Form.Field required>
       <label>닉네임</label>
-      <input placeholder='Nickname' />
+      <input placeholder='Nickname' onChange={onChangeEmail} />
     </Form.Field>
     <Form.Field>
       <label>전화번호</label>
-      <input placeholder='Tel. ex) 01012345678' />
+      <input placeholder='Tel. ex) 01012345678' onChange={onChangeEmail} />
     </Form.Field>
     <Form.Field>
       <label>주소</label>
-      <input placeholder='Address' />
+      <input placeholder='Address' onChange={onChangeEmail} />
     </Form.Field >
     <Button.Group widths='2'>
     <Button type='submit'>RESISTER</Button>
@@ -63,6 +117,7 @@ const DividerExampleVertical = () => (
 
   </Segment>
   </>
-)
+);
+};
 
-export default DividerExampleVertical
+export default Signup;

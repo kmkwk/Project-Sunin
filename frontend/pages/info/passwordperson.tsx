4404d6a2456de913c968e0691439f7c44ba99723
haru <jiaiha@naver.com>
2022-01-28 11:14:49 +0900

    feat:info password check

--- frontend/pages/info/passwordperson.tsx
+++ frontend/pages/info/passwordperson.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid, Header, Divider, Form, Button } from 'semantic-ui-react'
 import styles from "../../styles/signup.module.css"
 import Navbar from '../../src/component/Navbar'
@@ -6,7 +6,38 @@ import Menubar from '../../src/component/Menubar'
 import Link from 'next/link'
 
 
-const HeaderExampleContent = () => (
+const Passwordperson = () => {
+
+  const [currentpassword, setCurrentPassword] = useState("");
+  const [newpassword, setNewPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+
+  const onSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    if (newpassword !== confirmpassword) {
+      return setPasswordError(true);
+    }
+    console.log({
+      currentpassword,
+      newpassword,
+      confirmpassword,
+    });
+  };
+    const onChangeCurrentPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setCurrentPassword(event.target.value);
+    };
+    const onChangeNewPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setNewPassword(event.target.value);
+    };
+    const onChangeConfirmPassword = (
+      event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+      setPasswordError(event.target.value !== newpassword);
+      setConfirmPassword(event.target.value);
+    };
+
+return (
   <>
   <Navbar/>
 
@@ -20,18 +51,34 @@ const HeaderExampleContent = () => (
         <Divider />
         </div>
         <div className={ styles.formalign }>
-        <Form>
-        <Form.Field>
+        <Form onSubmit={onSubmit}>
+        <Form.Field required>
           <label>현재 비밀번호</label>
-          <input placeholder='Current Password' />
+          <Form.Input 
+          placeholder='Current Password'
+          name='currentpassword'
+          value={currentpassword}
+          onChange={onChangeCurrentPassword} />
         </Form.Field>
-        <Form.Field>
+        <Form.Field required>
           <label>새 비밀번호</label>
-          <input placeholder='New Password' />
+          <input placeholder='New Password'
+          name='newpassword'
+          value={newpassword}
+          onChange={onChangeNewPassword} />
         </Form.Field>
-        <Form.Field>
+        <Form.Field required>
           <label>새 비밀번호 확인</label>
-          <input placeholder='Confirm New Passwrod' />
+          <input placeholder='Confirm New Passwrod'
+            name="confirmpassword"
+            value={confirmpassword}
+            type="password"
+            onChange={onChangeConfirmPassword} />
+            {passwordError && (
+              <div style={{ color: "red" }}>
+                비밀번호가 일치하지 않습니다.
+               </div>
+                )}
         </Form.Field>
 
         <Button type='submit'>비밀번호 변경</Button>
@@ -47,6 +94,8 @@ const HeaderExampleContent = () => (
   </Grid>
   
  </>
-)
+);
+};
 
-export default HeaderExampleContent
+
+export default Passwordperson;

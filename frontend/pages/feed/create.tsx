2018d5bcf3d93d2bfa75c90dff1b006fce0607a3
haru <jiaiha@naver.com>
2022-02-09 22:49:26 +0900

    refactor:create 이미지 업로드 test

--- frontend/pages/feed/create.tsx
+++ frontend/pages/feed/create.tsx
@@ -1,8 +1,11 @@
-import { Button, Form, Input, Grid, TextArea } from "semantic-ui-react";
+import { Button, Form, Input, Image, Grid, TextArea } from "semantic-ui-react";
 import { useState } from "react";
 import Navbar from "../../src/component/Navbar";
 import styles from "../../styles/CreateFeed.module.css";
 import http from "../../src/lib/customAxios";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import SwiperCore, { Navigation } from "swiper";
 
 export default function Createfeed() {
   const [feed, setFeed] = useState({
@@ -11,7 +14,14 @@ export default function Createfeed() {
     filePath: [], // 이미지, 동영상
     hashtags: [], // 해시태그
   });
-
+  SwiperCore.use([Navigation]);
+  const [swiper, setSwiper] = useState(null);
+  const swiperParams = {
+    navigation : true,
+    onSwiper : setSwiper,
+  }
+  const [attachment,setAttachment] = useState()
+  const [createObjectURL, setCreateObjectURL] = useState(null);
   const handleOnChange = (e: any) => {
     setFeed({
       ...feed,
@@ -26,6 +36,21 @@ export default function Createfeed() {
       [e.target.name]: e.target.value,
       filePath: Array.from(e.target.files),
     });
+    const {
+      target: { files },
+    } = e;
+    const theFile = files[0];
+    const reader = new FileReader();
+
+   reader.onloadend = (finishedEvent: any) => {
+    setAttachment(finishedEvent.currentTarget['result']);
+    };
+    if (theFile) {
+    reader.readAsDataURL(theFile);
+  } else {
+    setAttachment(undefined)
+  }
+
   };
 
   const handleSubmit = (event: any) => {
@@ -55,6 +80,12 @@ export default function Createfeed() {
       });
   };
 
+  const onClearAttachment = () => {
+    const erase: any = document.getElementsByName('myImage') 
+    erase[''] = null
+  setAttachment(undefined);
+}
+
   return (
     <>
       <Navbar />
@@ -76,12 +107,21 @@ export default function Createfeed() {
               <Form.Field>
                 <div>
                   <h3>이미지 선택</h3>
+                  <Image src={createObjectURL} width="100px" />
                   <input
                     type="file"
                     accept="image/*, video/*"
                     multiple
                     onChange={uploadFile}
                   />
+                        {attachment && (
+        <div>
+          <br/>
+        <img src={attachment} width="80%" height="80%"/>
+        
+        <Button basic color='grey' onClick={onClearAttachment} fluid>Cancel</Button>
+        </div>
+      )}
                 </div>
               </Form.Field>
               {/* <Form.Field
@@ -91,10 +131,16 @@ export default function Createfeed() {
                 name="hashtags"
                 onChange={handleOnChange}
               /> */}
-              <Form.Field></Form.Field>
-              <Form.Field control={Button} onClick={handleSubmit}>
-                저장하기
+              <Form.Field>
+                <Swiper className={styles.swiperStyle}>
+                {/* <Swiper className={styles.swiperStyle} {...swiperParams} ref={setSwiper}> */}
+                  <SwiperSlide>1</SwiperSlide>
+                  <SwiperSlide>2</SwiperSlide>
+                  <SwiperSlide>3</SwiperSlide>
+                </Swiper>
               </Form.Field>
+              <Button basic color='black' onClick={handleSubmit} fluid>저장하기</Button>
+
             </Form>
           </Grid.Column>
           <Grid.Column width={3}></Grid.Column>

da0922e11623adc7fd6aafc30477badcb47a2377
오윤진 <funnyjin710@gmail.com>
2022-02-10 21:13:54 +0900

    refactor: chattingRoom

--- Backend/src/main/java/com/ssafy/sunin/controller/RoomForm.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.sunin.controller;
-
-
-import lombok.Data;
-
-@Data
-public class RoomForm {
-    private String name;
-}
--- Backend/src/main/java/com/ssafy/sunin/repository/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.sunin.repository;
-
-import com.ssafy.sunin.domain.chat.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}

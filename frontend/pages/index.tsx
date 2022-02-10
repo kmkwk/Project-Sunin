91d75a08f35f7c599412f4b001b50f3146ef04f5
choidawoon <choidawoon960717@gmail.com>
2022-02-11 00:46:33 +0900

    fix chat

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,54 +1,102 @@
-import type { NextPage } from 'next'
-// import Head from 'next/head'
-// import Image from 'next/image'
-import Menuvar from '../src/component/Menubar'
-import Navbar from '../src/component/Navbar'
-// import styles from '../styles/Home.module.css'
-import { Grid } from 'semantic-ui-react'
-import Axios from "axios"
-import { useEffect, useState } from 'react';
-import SampleFeedList from '../src/component/SampleFeedList'
-// import Searchbar from '../src/component/Searchbar'
+import { useEffect, useState } from "react";
+import SockJS from "sockjs-client";
+import Stomp from "webstomp-client";
+import styles from '../styles/chat.module.css'
 
-const Home: NextPage = () => {
-  const [list, setList] = useState([]);
+function Index() {
+  const SERVER_URL = "http://localhost:8080";
+  // const stompClient = Stomp.over(new SockJS(SERVER_URL));
 
-  const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+  const fixname = '고정이름'
 
-  function getData() {
-    Axios.get(API_URL)
-    .then(res => {
-      setList(res.data)
-    })
-  }
+  const [list, setList]: any = useState({
+    stompClient: Stomp.over(new SockJS(SERVER_URL)),
+    userName: "",
+    message: "",
+    recvList: [],
+  });
 
-  useEffect(()=>{
-    getData();
+  useEffect(() => {
+    console.log(`소켓 연결을 시도합니다. 서버 주소: ${SERVER_URL}`);
+    list.stompClient.connect(
+      {},
+      (frame: any) => {
+        // 소켓 연결 성공
+        list.stompClient.connected = true;
+        console.log("소켓 연결 성공", frame);
+        list.stompClient.subscribe("/send", (res: any) => {
+          console.log("구독으로 받은 메시지 입니다.", res.body);
+          list.recvList.push(JSON.parse(res.body));
+          setList({
+            ...list,
+            message:""
+          });
+        });
+      },
+      (error: any) => {
+        console.log("소켓 연결 실패", error);
+        list.stompClient.connected = false;
+      }
+    );
   }, []);
 
+  const handleOnChange = (e: any) => {
+    setList({
+      ...list,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  function sendMessage(e: any) {
+    if (e.key === "Enter" && list.userName !== "6" && list.message !== "") {
+      send();
+      const nametag: any = document.getElementsByName('userName')[0]
+      // nametag['value'] = ''
+      e.target.value = "";
+    }
+  }
+
+  function send() {
+    console.log("Send message:" + list.message);
+    if (list.stompClient && list.stompClient.connected) {
+      const msg = {
+        userName: fixname,
+        content: list.message,
+      };
+      list.stompClient.send("/receive", JSON.stringify(msg), {});
+    }
+    console.log(list.recvList);
+  }
+
   return (
-    <>
-      <Navbar />
-      {/* <Searchbar /> */}
-      <Grid columns={4} padded stackable>
-      <Grid.Column>
-        <Menuvar />
-      </Grid.Column>
-      <Grid.Column>
-        <b>Recent</b>
-        <SampleFeedList list={list.slice(0,3)} />
-      </Grid.Column>
-      <Grid.Column>
-        <b>Personal</b>
-        <SampleFeedList list={list.slice(3,6)} />
-      </Grid.Column>
-      <Grid.Column>
-        <b>Company</b>
-        <SampleFeedList list={list.slice(6,9)} />
-      </Grid.Column>
-    </Grid>
-    </>
-  )
+    <div>
+      <header className={styles.chat_wrap}>
+      {list.recvList.map((data: any, index: any) => {
+        return (
+          <h3 key={index} className={styles.back}>
+            유저이름: {data.userName} / 내용: {data.content}
+          </h3>
+        );
+      })}
+      </header>
+      {/* <span>유저이름: </span> */}
+
+      {/* <input name="userName" type="text" onChange={handleOnChange} /> */}
+      
+
+      <div className={styles.foot}>
+
+        <span>내용: </span>
+        <input
+          name="message"
+          type="text"
+          onChange={handleOnChange}
+          onKeyUp={sendMessage} />
+
+        </div>
+    </div>
+  );
 }
 
-export default Home
+export default Index;
+

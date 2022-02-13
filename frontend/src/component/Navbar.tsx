653ba7ce609d9b0a6726d5e60b81afe683b007fa
오윤진 <funnyjin710@gmail.com>
2022-02-13 14:27:38 +0900

    test:navbar-loginbutton

--- frontend/src/component/Navbar.tsx
+++ frontend/src/component/Navbar.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
   
   return (
     <div>
-      <Menu pointing secondary>
+      <Menu stackable pointing secondary>
         <Menu.Item>
           <Link href="/"><a><img className={ styles.logo_size } src='/images/로고center.png' /></a></Link>
         </Menu.Item>
@@ -54,11 +54,12 @@ export default function Navbar() {
           <Alarm />
         </Menu.Item> 
         :
-        <Menu.Item
-          name='signup'
-        >
-          <Link href="/signup"><a  className={ styles.nav_names }>Signup</a></Link>
-        </Menu.Item>
+        // <Menu.Item
+        //   name='signup'
+        // >
+        //   <Link href="/signup"><a  className={ styles.nav_names }>Signup</a></Link>
+        // </Menu.Item>
+        ""
         }
 
         {isLogin?
@@ -68,12 +69,15 @@ export default function Navbar() {
           <Link href="/"><a onClick={goLogout} className={ styles.nav_logout }>Logout</a></Link>
         </Menu.Item>
         :
-        <Menu.Item
-          name='login' className={ styles.nav_login }
-        >
+        <div className={ styles.nav_loginbutton }>
+          <Menu.Item
+           name='login' className={ styles.nav_login }
+          >
           {/* <Link href="/login"><a>Login</a></Link> */}
           <LoginModal />
         </Menu.Item>
+        </div>
+        
         }
       </Menu> 
     </div>

bf65590ced5cf9234091c012b002f3fceb339218
aff4110 <aff4110@gmail.com>
2022-02-15 19:59:02 +0900

    fix: 빌드에러 수정

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -14,7 +14,6 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-
 const Home: NextPage = () => {
   const [list, setList]: any = useState([
     "images/1번.jpg",
@@ -25,11 +24,11 @@ const Home: NextPage = () => {
   return (
     <>
       <Navbar />
-      
+
       <Swiper
-      // install Swiper modules
-        style={{ width:"auto", height: "auto"}}
-        autoplay={{ delay: 3000 }} 
+        // install Swiper modules
+        style={{ width: "auto", height: "auto" }}
+        autoplay={{ delay: 3000 }}
         modules={[Navigation, Pagination, Autoplay]}
         spaceBetween={0}
         slidesPerView={1}
@@ -37,11 +36,20 @@ const Home: NextPage = () => {
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-      >
-        <SwiperSlide><img src="images/1번.jpg" width={"100%"} height={"100%"}/></SwiperSlide>
-        <SwiperSlide><a href="http://localhost:3000/feed"><img src="images/2번.png" width={"100%"} height={"100%"}/></a></SwiperSlide>
-        <SwiperSlide><a href="https://www.youtube.com/" target="_blank"><img src="images/3번.jpg" width={"100%"} height={"100%"}/></a></SwiperSlide>
+        onSlideChange={() => console.log("slide change")}>
+        <SwiperSlide>
+          <img src="images/1번.jpg" width={"100%"} height={"100%"} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <a href="http://localhost:3000/feed">
+            <img src="images/2번.png" width={"100%"} height={"100%"} />
+          </a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a href="https://www.youtube.com/">
+            <img src="images/3번.jpg" width={"100%"} height={"100%"} />
+          </a>
+        </SwiperSlide>
       </Swiper>
 
       <body>
@@ -53,9 +61,9 @@ const Home: NextPage = () => {
               </Grid.Column>
               <Grid.Column>
                 {/* <br /><br /> */}
-                <br/>
-                <div  className={styles.title}>
-                선한 영향력 프로젝트 썬인🌍🌞입니다.
+                <br />
+                <div className={styles.title}>
+                  선한 영향력 프로젝트 썬인🌍🌞입니다.
                 </div>
                 <br />
                 우리 함께 SunIn해봐요!
@@ -65,64 +73,54 @@ const Home: NextPage = () => {
         </div>
         {/* <div className={styles.container}> */}
         <div className={styles.mainfirst}>
-          
           <br />
           {/* <br /> */}
 
-          <div  className={styles.title}>
-          선한 영향력을 행사하셨나요?
-          </div>
+          <div className={styles.title}>선한 영향력을 행사하셨나요?</div>
           <></>
           <br />
-          <div  className={styles.content}>
-          자신의 피드에 사진, 태그와 함께
-          <br/>
-          피드에 기록을 남겨보세요
+          <div className={styles.content}>
+            자신의 피드에 사진, 태그와 함께
+            <br />
+            피드에 기록을 남겨보세요
           </div>
-        
+
           {/* </div> */}
         </div>
         <div className={styles.mainsecond}>
           <br />
           {/* <br /> */}
-          <div  className={styles.title}>
-          SunIn 아이콘을 성장시켜보세요
-          </div>
-        
+          <div className={styles.title}>SunIn 아이콘을 성장시켜보세요</div>
+
           <br />
 
           <div className={styles.content}>
-          썬인 활동에 따라 성장하는 아이콘을 보며 성취감을 느껴보세요
+            썬인 활동에 따라 성장하는 아이콘을 보며 성취감을 느껴보세요
           </div>
-
-          
         </div>
         <div className={styles.mainthird}>
           <br />
           {/* <br /> */}
-          <div  className={styles.title}>
-          함께 SunIn할 친구를 찾으시나요?
-          </div>
+          <div className={styles.title}>함께 SunIn할 친구를 찾으시나요?</div>
           <br />
-          <div  className={styles.content}>
-          팔로우 및 채팅을 통해 관심있는 사람과 소통할 수 있습니다.
+          <div className={styles.content}>
+            팔로우 및 채팅을 통해 관심있는 사람과 소통할 수 있습니다.
           </div>
         </div>
       </body>
 
       <footer>
-      <div className={styles.footer}>
-      <span className={styles.contact}>CONTACT US</span>
-      <br/>
-      🗺️ SunIn / 썬인
-      <br/>
-      광주광역시 광산구 하남산단 6번로 107
-      <br/>
-      ✉️ SunInOffical@gmail.com
-      <br/>
-      </div>
-      <div className={styles.distributed}>© Distributed By SunIn</div>
-      
+        <div className={styles.footer}>
+          <span className={styles.contact}>CONTACT US</span>
+          <br />
+          🗺️ SunIn / 썬인
+          <br />
+          광주광역시 광산구 하남산단 6번로 107
+          <br />
+          ✉️ SunInOffical@gmail.com
+          <br />
+        </div>
+        <div className={styles.distributed}>© Distributed By SunIn</div>
       </footer>
     </>
   );

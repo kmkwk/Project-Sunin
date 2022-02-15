5d65aa8bebf7b581027ffe50694563e258d770a9
haru <jiaiha@naver.com>
2022-02-15 16:40:21 +0900

    feat:footer

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -16,6 +16,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import GetUserData from "src/lib/getUserData";
 
 const Home: NextPage = () => {
   const [list, setList]: any = useState([
@@ -48,34 +49,66 @@ const Home: NextPage = () => {
           </Grid>
         </div>
         {/* <div className={styles.container}> */}
-        <div className={styles.container}>
+        <div className={styles.mainfirst}>
+           
           <br />
           {/* <br /> */}
+ 
+          <div  className={styles.title}>
           선한 영향력을 행사하셨나요?
+          </div>
+          <></>
           <br />
-          <br />
-          자신의 피드에 사진, 태그와 함께 피드에 기록을 남겨보세요
+          <div  className={styles.content}>
+          자신의 피드에 사진, 태그와 함께
+          <br/>
+           피드에 기록을 남겨보세요
+          </div>
+        
           {/* </div> */}
         </div>
         <div className={styles.mainsecond}>
           <br />
           {/* <br /> */}
+          <div  className={styles.title}>
           SunIn 아이콘을 성장시켜보세요
+          </div>
+        
           <br />
-          <br />
+
+          <div className={styles.content}>
           썬인 활동에 따라 성장하는 아이콘을 보며 성취감을 느껴보세요
+          </div>
+
+          
         </div>
         <div className={styles.mainthird}>
           <br />
           {/* <br /> */}
+          <div  className={styles.title}>
           함께 SunIn할 친구를 찾으시나요?
+          </div>
           <br />
-          <br />
+          <div  className={styles.content}>
           팔로우 및 채팅을 통해 관심있는 사람과 소통할 수 있습니다.
+          </div>
         </div>
       </body>
 
-      <footer>footer 내용입니다.</footer>
+      <footer>
+      <div className={styles.footer}>
+      <span className={styles.contact}>CONTACT US</span>
+      <br/>
+      🗺️ SunIn / 썬인
+      <br/>
+      광주광역시 광산구 하남산단 6번로 107
+      <br/>
+      ✉️ SunInOffical@gmail.com
+      <br/>
+      </div>
+      <div className={styles.distributed}>© Distributed By SunIn</div>
+      
+      </footer>
     </>
   );
 };

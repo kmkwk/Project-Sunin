e30d4e4019c2f29847e5edaf8e45b618ba3f60b7
김경민 <nutmy52@naver.com>
2022-02-16 11:26:36 +0900

    feature/푸시 알람 테스트

--- Backend/src/main/java/com/ssafy/sunin/controller/MessageController.java
+++ Backend/src/main/java/com/ssafy/sunin/controller/MessageController.java
@@ -8,15 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.nio.file.Path;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/alram")
 @RequiredArgsConstructor
 @Slf4j
 public class MessageController {
@@ -24,21 +20,54 @@ public class MessageController {
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final AlarmService alarmService;
 
-    @MessageMapping("/send/{fromUserId}/{toUserId}/{messages}")
-    public void message(@DestinationVariable("fromUserId") Long fromUserId,
+    @MessageMapping("/send/{fromUserId}/{toUserId}/{messages}/{feedId}")
+    public void messageLike(
+            @DestinationVariable("fromUserId") Long fromUserId,
                         @DestinationVariable("toUserId") Long toUserId,
-                        @DestinationVariable("messages") String messages) throws InterruptedException {
-        Thread.sleep(1000);
-        if(!fromUserId.equals(toUserId)){
-            alarmService.writeMessage(fromUserId,toUserId,messages);
-            simpMessagingTemplate.convertAndSend("/sub/"+toUserId, messages);
+                        @DestinationVariable("messages") String messages,
+                        @DestinationVariable("feedId") String feedId) {
+
+        if(!fromUserId.equals(toUserId)) {
+            //좋아요를 누른사람한테는 메시지를 보내면 안됨
+            if (alarmService.getLikeUser(fromUserId, feedId)) {
+                alarmService.writeMessage(fromUserId, toUserId, messages);
+                simpMessagingTemplate.convertAndSend("/sub/" + toUserId, messages);
+            }
         }
     }
+
+    @MessageMapping("/send/{fromUserId}/{toUserId}/{messages}")
+    public void messageFollower(
+            @DestinationVariable("fromUserId") Long fromUserId,
+            @DestinationVariable("toUserId") Long toUserId,
+            @DestinationVariable("messages") String messages) {
+
+        if(!fromUserId.equals(toUserId)) {
+            //이미 팔로워가 되있으면 메시지를 보내면 안됨
+            if (alarmService.getFollower(fromUserId, toUserId) == null) {
+                alarmService.writeMessage(fromUserId, toUserId, messages);
+                simpMessagingTemplate.convertAndSend("/sub/" + toUserId, messages);
+            }
+        }
+    }
+
+//    @MessageMapping("/send/{fromUserId}/{toUserId}/{messages}/{feedId}")
+//    public void messageComment(
+//            @DestinationVariable("fromUserId") Long fromUserId,
+//            @DestinationVariable("toUserId") Long toUserId,
+//            @DestinationVariable("messages") String messages,
+//            @DestinationVariable("feedId") String feedId) {
+//
+//        if(!fromUserId.equals(toUserId)) {
+//            alarmService.writeMessage(fromUserId, toUserId, messages);
+//            simpMessagingTemplate.convertAndSend("/sub/" + toUserId, messages);
+//        }
+//    }
 
     @GetMapping("/{toUserId}")
     public ResponseEntity<List<AlarmResponse>> getMessage(@PathVariable("toUserId") Long toUserId){
         log.info("getMessage");
-
+        System.out.println(toUserId);
         return ResponseEntity.ok(alarmService.getMessage(toUserId));
     }
 
